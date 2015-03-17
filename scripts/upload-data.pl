5dbbe2ce46e4ccac5aaa37740098c4decf62f065
Max <max.shatsky@gmail.com>
2015-03-17 14:07:06 -0700

    version 0.4

--- scripts/upload-data.pl
+++ scripts/upload-data.pl
@@ -315,14 +315,14 @@ createObjectsForMissingRefs($serv, $workspace, \%BrseqRes2objref);
 
 
 my $objs = $serv->get_object_subset(
-    [ ( { ref => $genome_ref,
-	  included => "/features/7" 
-	},
-	{ ref => $genome_ref,
-	  included => "/scientific_name"
-	}
-      ) ]
-    );
+    [ { ref => $genome_ref,
+	included => "/features/7" 
+      },
+      { ref => $genome_ref,
+	included => "/scientific_name"
+      }
+    ] 
+);
 
 foreach my $obj (@{$objs}){			      
     foreach ( @{$obj->{info}} ){
